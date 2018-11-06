2c1d40c3b6f65d51583f1048fb95c6958857f18c
Jakob Dozier <jakob.dozier@gmail.com>
2018-11-06 14:16:29 -0600

    added firebaseauth

--- jakob/Powwow/app/src/main/java/com/example/jakobdozier/powwow/RegisterActivity.java
+++ jakob/Powwow/app/src/main/java/com/example/jakobdozier/powwow/RegisterActivity.java
@@ -1,43 +1,111 @@
 package com.example.jakobdozier.powwow;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText fName;
-    EditText lName;
-    EditText email;
-    Button register;
-    FloatingActionButton back;
+    private EditText fName;
+    private EditText lName;
+    private EditText emailRegister;
+    private EditText passwordRegister;
+    private Button register;
+    private FloatingActionButton back;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        fName = (EditText) findViewById(R.id.editText_fName);
-        lName = (EditText) findViewById(R.id.editText_lName);
-        email = (EditText) findViewById(R.id.editText_email);
+        fName = findViewById(R.id.editText_fName);
+        lName = findViewById(R.id.editText_lName);
+        emailRegister = findViewById(R.id.editText_email);
+        passwordRegister = findViewById(R.id.editText_password);
 
-        register = (Button) findViewById(R.id.button_register);
+        register = findViewById(R.id.button_register);
 
-        back = (FloatingActionButton) findViewById(R.id.floatingActionButton_back);
+        back = findViewById(R.id.floatingActionButton_back);
+
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         register.setOnClickListener(this);
         back.setOnClickListener(this);
+    }
+
+    public void registerUser() {
+        String firstName = fName.getText().toString().trim();
+        String lastName = lName.getText().toString().trim();
+        String email = emailRegister.getText().toString().trim();
+        String password = passwordRegister.getText().toString().trim();
+
+        if (TextUtils.isEmpty(firstName)) {
+            //email is empty
+            Toast.makeText(this, "Please enter your first name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(lastName)) {
+            //password is empty
+            Toast.makeText(this, "Please enter your last name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            //email is empty
+            Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            //password is empty
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this, "Unable to Register", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_register:
+                registerUser();
                 break;
             case R.id.floatingActionButton_back:
                 startActivity(new Intent(this, LoginActivity.class));
